88ae7e61fb913226392fbc4f2fd2be3400c47395
Trevor Whitley <43686926+trevorwhitley@users.noreply.github.com>
2018-10-22 19:05:11 -0700

    Add files via upload

---
+++ HackerRankDay0.py
@@ -0,0 +1,15 @@
+#Name: HackerRankDay0
+#Purpose: Prints 'Hello World'
+#Author: Trevor Whitley
+#Date 10/21/18
+
+# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
+input_string = input()
+
+# Print a string literal saying "Hello, World." to stdout.
+print('Hello, World.')
+
+# TODO: Write a line of code here that prints the contents of input_string to stdout.
+
+print(input_string)
+        
---
+++ HackerRankDay1.py
@@ -0,0 +1,20 @@
+#Name: HackerRankDay1.py
+#Purpose: Learn data types.
+#Author: Trevor Whitley
+#Date: 10/22/18
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+i2 = input()
+d2 = input()
+s2 = input()
+# Print the sum of both integer variables on a new line.
+print(int(i) + int(i2))
+# Print the sum of the double variables on a new line.
+print(float(d) + float(d2))
+# Concatenate and print the String variables on a new line
+print(str(s) + str(s2))
+# The 's' variable above should be printed first.
